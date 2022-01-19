3e6176dfaa25fb084d9f690a9b3d755ebe1bd3a1
alvaroun <alvaromachadoferreira@hotmail.com>
2022-01-18 22:37:34 -0300

    utilizando os repositories

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import Class from '../models/Class';
+import ClassRepository from '../repositories/ClassRepository';
 
 const classRouter = Router();
 
@@ -12,6 +13,16 @@ classRouter.post('/', async (request, response) => {
   } catch (err) {
     console.log('err.message :>> ', err.message);
   }
+});
+
+classRouter.get('/', async (request, response) => {
+  response.json(await getRepository(Class).find());
+});
+
+classRouter.get('/:name', async (request, response) => {
+  const repository = getCustomRepository(ClassRepository);
+  const res = await repository.findByName(request.params.name);
+  response.json(res);
 });
 
 export default classRouter;
